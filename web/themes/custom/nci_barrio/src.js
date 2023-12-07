99ce6422bdd309370a0eaccce8272d337a0ad5f4
smandava1212 <smandava1212@gmail.com>
2023-12-07 08:38:18 -0700

    adding styles for bridg home page

--- web/themes/custom/nci_barrio/src.js
+++
@@ -1,7 +0,0 @@
-import './scss/main.scss';
-
-/**
- * Imports the NCI Extended Header with Mega Menu component auto initializer.
- * Note: this should output a console warning for mega menu.
- */
-import '@nciocpl/ncids-js/components/nci-header/extended-with-mega-menu/auto-init';
